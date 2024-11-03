6354474e357b8364b84806c3ae445fb135664de9
Yoodlo_ <125147570+Yoodlo@users.noreply.github.com>
2024-11-03 19:42:03 +0100

    structuring the html and css elements

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 // script of TestCPS.html
+
+let ttClickOut = document.getElementById("ttClickOut");
+let clickPerSecOut = document.getElementById("clickPerSecOut");
+let clickBoxLabel = document.getElementById("clickBoxLabel");
+let timerSpanIn = document.getElementById("timerSpanIn");
+let gameRunning = new Boolean(false)
+let ttclick = 0;
+let chronometer = 0;
+
+function newClick() {
+	if (gameRunning == true) {
+		ttclick ++;
+	} else {
+		startGame();
+	}
+}
+
+function startGame() {
+	gameRunning = true;
+	timerSpan = timerSpanIn.value
+	createChronometer(timerSpan);
+}
+
+function createChronometer(timerSpan) {
+    let currentTime = 0;
+    let intervalId = null;
+}
+
+function start() {
+        if (intervalId) return;
+        
+        intervalId = setInterval(() => {
+            currentTime += 0.1;
+            
+            console.log(currentTime.toFixed(1) + "s");
+            
+            if (currentTime >= timerSpan) {
+                stop();
+            }
+        }, 100);
+    }
+
