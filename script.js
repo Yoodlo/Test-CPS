c9bc4681a8a25cb884dc945f71c9c2493c622cf0
Yoodlo_ <125147570+Yoodlo@users.noreply.github.com>
2024-11-04 17:35:00 +0100

    introduce the game mecanics

--- script.js
+++ script.js
@@ -7,37 +7,64 @@ let timerSpanIn = document.getElementById("timerSpanIn");
 let gameRunning = new Boolean(false)
 let ttclick = 0;
 let chronometer = 0;
+let timeout = new Boolean(false);
+let clickPerSec = 0;
 
 function newClick() {
-	if (gameRunning == true) {
-		ttclick ++;
+	console.log("*click*")
+	if (timeout == false) {
+		if (gameRunning == true) {
+			ttclick ++;
+			ttClickOut.value = ttclick;
+		} else {
+			ttclick = 0;
+			startGame();
+		}
 	} else {
-		startGame();
+		return;
 	}
 }
 
+function restart() {
+	timeout = false;
+}
 function startGame() {
+	console.log("game started")
 	gameRunning = true;
 	timerSpan = timerSpanIn.value
-	createChronometer(timerSpan);
+	chronometer = createChronometer(timerSpan);
+	chronometer.start();
 }
 
 function createChronometer(timerSpan) {
     let currentTime = 0;
     let intervalId = null;
-}
-
-function start() {
+    
+    function start() {
         if (intervalId) return;
         
         intervalId = setInterval(() => {
             currentTime += 0.1;
-            
+			clickBoxLabel.innerText = currentTime.toFixed(1);
+			clickPerSec = ttclick / currentTime.toFixed(1);
+            clickPerSecOut.value = clickPerSec.toFixed(1);
             console.log(currentTime.toFixed(1) + "s");
             
             if (currentTime >= timerSpan) {
                 stop();
             }
         }, 100);
+	}
+
+    function stop() {
+        clearInterval(intervalId);
+        intervalId = null;
+		clickBoxLabel.innerText = "Restart";
+		gameRunning = false;
+		timeout = true;
+		alert("You clicked " + ttclick + " in " + timerSpan + " s. That's egal to " + clickPerSec.toFixed(1) + " clicks/sec.")
+		setTimeout(restart, 3000)
     }
 
+    return { start, stop };
+}
