8a421ac48742742b444335251bb83404394a8e89
Yoodlo_ <125147570+Yoodlo@users.noreply.github.com>
2024-12-07 19:47:39 +0100

    updating the navigation tab

--- script-aimTrain.js
+++ script-aimTrain.js
@@ -2,7 +2,19 @@
 
 let targetButton = document.getElementById("targetButton");
 let modeSwitch = document.getElementById("modeSwitch");
+let navigationBar = document.getElementById("navigationBar");
+let navigationBarArrow = document.getElementById("navigationBarArrow");
+let popUpTextBox = document.getElementById("popUpTextBox");
 let gameMode = "time";
+let navState = "close";
+
+navigationBar.addEventListener("mouseover", function(){
+  navigationBarArrow.classList.add("arrowHovered");
+});
+
+navigationBar.addEventListener("mouseout", function(){
+  navigationBarArrow.classList.remove("arrowHovered");
+});
 
 function randX(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);;
@@ -26,13 +38,30 @@ function pressed() {
 function switchMode() {
 	console.log("switching mode");
 	if (gameMode == "time") {
-		modeSwitch.value = "click";
+		modeSwitch.value = "GameMode : Click";
 		gameMode = "click";
 	} else if (gameMode == "click") {
-		modeSwitch.value = "time";
+		modeSwitch.value = "GameMode : Time";
 		gameMode = "time";
 	} else {
-		modeSwitch.value = "time"
+		modeSwitch.value = "GameMode : Time"
 		gameMode = "time";
+	}
+}
+
+function openNavBox() {
+	console.log("showing the navigation");
+	if (navState == "close") {
+		navigationBar.style.height = "100px";
+		popUpTextBox.hidden = false;
+		navState = "open";
+	} else if (navState == "open") {
+		navigationBar.style.height = "29px";
+		popUpTextBox.hidden = true;
+		navState = "close";
+	} else {
+		navigationBar.style.height = "29px";
+		popUpTextBox.hidden = false;
+		navState = "close";
 	}
 }
